afcc7e326c835951fcbbcd0653c83f6a17f7d026
xuzx007 <40702607+xuzx007@users.noreply.github.com>
2020-01-30 10:35:38 +0100

    upload pwm fan py

---
+++ easy_pwm_fan.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+
+fan_pin = 12 #BCM number
+delaystart = 5
+delay = 20 #
+pwm_freq = 25 #Hz
+
+dutyCycleLevel00 = 0
+spinFanLevel01 = 45
+dutyCycleLevel01 = 30
+spinFanLevel02 = 55
+dutyCycleLevel02 = 60
+spinFanLevel03 = 60
+dutyCycleLevel03 = 100
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(fan_pin, GPIO.OUT, initial=GPIO.LOW)
+fan = GPIO.PWM(fan_pin, pwm_freq)
+fan.start(100) # dutyCycle
+time.sleep(delaystart)
+
+try:
+    while(1):
+         cpuTempFile = open("/sys/class/thermal/thermal_zone0/temp", "r")
+         cpuTemp = float(cpuTempFile.read()) / 1000
+         cpuTempFile.close()
+
+         if cpuTemp <= spinFanLevel01:
+             fan.ChangeDutyCycle(dutyCycleLevel00)
+         elif spinFanLevel01 < cpuTemp <= spinFanLevel02:
+             fan.ChangeDutyCycle(dutyCycleLevel01)
+         elif spinFanLevel02 < cpuTemp <= spinFanLevel03:
+             fan.ChangeDutyCycle(dutyCycleLevel02)
+         else:
+             fan.ChangeDutyCycle(dutyCycleLevel03)
+         time.sleep(delay)
+
+except KeyboardInterrupt:
+    print("Fan ctrl interrupted by keyboard")
+    GPIO.cleanup()
+    sys.exit()
